c83de08511267e70a27773d28ea55d457b9f2cda
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2025-03-25 12:44:08 +0300

    Check whether FS is case sensitive

--- zotfile_doctor.py
+++ zotfile_doctor.py
@@ -18,17 +18,26 @@
 
 import argparse
 import os
-import pathlib
 import re
 import sqlite3
+import tempfile
 import unicodedata
+from functools import cache
+
+
+# https://stackoverflow.com/a/36580834
+@cache
+def is_fs_case_sensitive():
+    with tempfile.NamedTemporaryFile(prefix="TmP") as tmp_file:
+        return not os.path.exists(tmp_file.name.lower())
 
 
 def get_db_set(db, d):
     conn = sqlite3.connect(db)
 
     db_c = conn.execute(
-        'select path from itemAttachments where linkMode = 2 or linkMode = 3 and (contentType = "application/pdf" or contentType = "application/djvu") ')
+        'select path from itemAttachments where linkMode = 2 or linkMode = 3 and (contentType = "application/pdf" or contentType = "application/djvu") '
+    )
     db_d = db_c.fetchall()
 
     db_l = []
@@ -36,20 +45,23 @@ def get_db_set(db, d):
         try:
             # Ignore all kind of errors wholesale, i.e. duck typing
             item = db_d[i][0]
-            if not (item.lower().endswith(".pdf") or item.lower().endswith(".djvu")) :
+            if not (item.lower().endswith(".pdf") or item.lower().endswith(".djvu")):
                 continue
-            if item.count('attachments:') > 0: # relative path
-                item = item.replace('attachments:', "")
-            else: # absolute path
-                item = str(pathlib.Path(item).relative_to(d))
-        except:
+            if item.count("attachments:") > 0:  # relative path
+                item = item.replace("attachments:", "")
+            else:  # absolute path
+                item = os.path.relpath(item, start=d)
+            if not is_fs_case_sensitive():
+                item = item.lower()
+        except _:
             # file is not in zotfile directory
             continue
 
-        db_l.append(unicodedata.normalize("NFD", item))
+        db_l.append(unicodedata.normalize("NFD", item).replace("\\", "/"))
 
     db_set = set(db_l)
     return db_set
+
 
 def get_dir_set(d):
     rule = re.compile("(?s:.*\\.(pdf|djvu))\\Z", re.IGNORECASE)
@@ -58,10 +70,12 @@ def get_dir_set(d):
         for filename in [name for name in filenames if rule.match(name)]:
             matches.append(os.path.join(root, filename))
 
-    fs = [str(pathlib.Path(f).relative_to(d)) for f in matches]
-    fs = [unicodedata.normalize("NFD", x) for x in fs]
+    fs = [os.path.relpath(f, start=d) for f in matches]
+    fs = [unicodedata.normalize("NFD", x).lower() for x in fs]
+    fs = [x.replace("\\", "/") for x in fs]
     d_set = set(fs)
     return d_set
+
 
 def remove_empty_dirs(d):
     for root, dirnames, _filenames in os.walk(d, topdown=False):
@@ -71,6 +85,7 @@ def remove_empty_dirs(d):
             except OSError:
                 continue
 
+
 def main(db, d, clean=False):
     db_set = get_db_set(db, d)
     dir_set = get_dir_set(d)
@@ -78,10 +93,14 @@ def main(db, d, clean=False):
     db_not_dir = db_set.difference(dir_set)
     dir_not_db = dir_set.difference(db_set)
 
-    print(f"There were {len(db_not_dir)}/{len(db_set)} files in DB but not in zotfile directory:")
+    print(
+        f"There were {len(db_not_dir)}/{len(db_set)} files in DB but not in zotfile directory:"
+    )
     for f in sorted(db_not_dir):
         print("   " + f)
-    print(f"\nThere were {len(dir_not_db)}/{len(dir_set)} files in zotfile directory but not in DB:")
+    print(
+        f"\nThere were {len(dir_not_db)}/{len(dir_set)} files in zotfile directory but not in DB:"
+    )
     for f in sorted(dir_not_db):
         print("   " + f)
 
@@ -93,10 +112,16 @@ def main(db, d, clean=False):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="zotfile directory consistency checker")
+    parser = argparse.ArgumentParser(
+        description="zotfile directory consistency checker"
+    )
     parser.add_argument("zotero_sqlite", help="path-to-zotero/zotero.sqlite")
     parser.add_argument("zotfile_directory", help="zotfile directory")
-    parser.add_argument("-c", "--clean", action="store_true",
-                        help="remove files in zotfile directory but not in DB")
+    parser.add_argument(
+        "-c",
+        "--clean",
+        action="store_true",
+        help="remove files in zotfile directory but not in DB",
+    )
     args = parser.parse_args()
     main(args.zotero_sqlite, args.zotfile_directory, args.clean)
